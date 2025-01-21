1fe2a236059bf32e8829ae068e21dce553416c0c
sarthakcodes2203 <hellheaven006@gmail.com>
2025-01-21 20:17:47 +0530

    Add files via upload

---
+++ GleamClone/script.js
@@ -0,0 +1,179 @@
+<script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+        <script type="text/javascript">
+            var sbiajaxurl = "https://matchthemes.com/demowp/gleam/wp-admin/admin-ajax.php";
+        </script>
+        <script data-optimized="1" src="https://matchthemes.com/demowp/gleam/wp-content/litespeed/js/7f1e37d3bd7a7b54477e5ed4a043725a.js?ver=3725a" id="swv-js"></script>
+        <script type="text/javascript" id="contact-form-7-js-extra">
+            /*  */
+            var wpcf7 = {
+                "api": {
+                    "root": "https:\/\/matchthemes.com\/demowp\/gleam\/wp-json\/",
+                    "namespace": "contact-form-7\/v1"
+                },
+                "cached": "1"
+            };
+            /*  */
+        </script>
+        <script data-optimized="1" src="https://matchthemes.com/demowp/gleam/wp-content/litespeed/js/47d64fbd0ec76110d16c2e56db24c0e8.js?ver=4c0e8" id="contact-form-7-js"></script>
+        <script data-optimized="1" src="https://matchthemes.com/demowp/gleam/wp-content/litespeed/js/fab134eb992efe4942e24467a9065a16.js?ver=65a16" id="easing-js"></script>
+        <script data-optimized="1" src="https://matchthemes.com/demowp/gleam/wp-content/litespeed/js/b7f9714f060e0345526e43d2853ee39e.js?ver=ee39e" id="swiper-js"></script>
+        <script data-optimized="1" src="https://matchthemes.com/demowp/gleam/wp-content/litespeed/js/b679f895edf3f161b365bd7f4b62d543.js?ver=2d543" id="simple-lightbox-js"></script>
+        <script data-optimized="1" src="https://matchthemes.com/demowp/gleam/wp-content/litespeed/js/13352e754ff60af82de4e73d3bbfad9a.js?ver=fad9a" id="isotope-js"></script>
+        <script data-optimized="1" src="https://matchthemes.com/demowp/gleam/wp-content/litespeed/js/28fd7fd72f5c5b4e888b33ed79cb182b.js?ver=b182b" id="gleam-init-js"></script>
+        <script id="gleam-init-js-after">
+            /*  */
+            (function($) {
+                'use strict';
+
+                $(window).on('scroll', function() {
+                    if ($(document).scrollTop() > 1) {
+                        $('.main-header').addClass('nav-fixed-top');
+                    } else {
+                        $('.main-header').removeClass('nav-fixed-top');
+                    }
+
+                });
+
+            }
+            )(jQuery);
+            /*  */
+        </script>
+        <script data-optimized="1" defer src="https://matchthemes.com/demowp/gleam/wp-content/litespeed/js/6b9133e0e89333dc0a1367b60f73afcf.js?ver=3afcf" id="akismet-frontend-js"></script>
+        <script type="text/javascript" id="sbi_scripts-js-extra">
+            /*  */
+            var sb_instagram_js_options = {
+                "font_method": "svg",
+                "resized_url": "https:\/\/matchthemes.com\/demowp\/gleam\/wp-content\/uploads\/sb-instagram-feed-images\/",
+                "placeholder": "https:\/\/matchthemes.com\/demowp\/gleam\/wp-content\/plugins\/instagram-feed\/img\/placeholder.png",
+                "ajax_url": "https:\/\/matchthemes.com\/demowp\/gleam\/wp-admin\/admin-ajax.php"
+            };
+            /*  */
+        </script>
+        <script data-optimized="1" src="https://matchthemes.com/demowp/gleam/wp-content/litespeed/js/090579d3b93204db3dbd35286985af17.js?ver=5af17" id="sbi_scripts-js"></script>
+        <script data-optimized="1" src="https://matchthemes.com/demowp/gleam/wp-content/litespeed/js/1f4b7634557a76ded640af5981a2b3f0.js?ver=2b3f0" id="elementor-webpack-runtime-js"></script>
+        <script data-optimized="1" src="https://matchthemes.com/demowp/gleam/wp-content/litespeed/js/dc9e006b1c0f51b088f1cf5844eaa448.js?ver=aa448" id="elementor-frontend-modules-js"></script>
+        <script data-optimized="1" src="https://matchthemes.com/demowp/gleam/wp-content/litespeed/js/3877194282ad573d72e7f3f70432aeb6.js?ver=2aeb6" id="elementor-waypoints-js"></script>
+        <script data-optimized="1" src="https://matchthemes.com/demowp/gleam/wp-content/litespeed/js/ee32a93ea8a09f86fdf738ad3c5b4a52.js?ver=b4a52" id="jquery-ui-core-js"></script>
+        <script id="elementor-frontend-js-before">
+            /*  */
+            var elementorFrontendConfig = {
+                "environmentMode": {
+                    "edit": false,
+                    "wpPreview": false,
+                    "isScriptDebug": false
+                },
+                "i18n": {
+                    "shareOnFacebook": "Share on Facebook",
+                    "shareOnTwitter": "Share on Twitter",
+                    "pinIt": "Pin it",
+                    "download": "Download",
+                    "downloadImage": "Download image",
+                    "fullscreen": "Fullscreen",
+                    "zoom": "Zoom",
+                    "share": "Share",
+                    "playVideo": "Play Video",
+                    "previous": "Previous",
+                    "next": "Next",
+                    "close": "Close",
+                    "a11yCarouselWrapperAriaLabel": "Carousel | Horizontal scrolling: Arrow Left & Right",
+                    "a11yCarouselPrevSlideMessage": "Previous slide",
+                    "a11yCarouselNextSlideMessage": "Next slide",
+                    "a11yCarouselFirstSlideMessage": "This is the first slide",
+                    "a11yCarouselLastSlideMessage": "This is the last slide",
+                    "a11yCarouselPaginationBulletMessage": "Go to slide"
+                },
+                "is_rtl": false,
+                "breakpoints": {
+                    "xs": 0,
+                    "sm": 480,
+                    "md": 768,
+                    "lg": 1025,
+                    "xl": 1440,
+                    "xxl": 1600
+                },
+                "responsive": {
+                    "breakpoints": {
+                        "mobile": {
+                            "label": "Mobile Portrait",
+                            "value": 767,
+                            "default_value": 767,
+                            "direction": "max",
+                            "is_enabled": true
+                        },
+                        "mobile_extra": {
+                            "label": "Mobile Landscape",
+                            "value": 880,
+                            "default_value": 880,
+                            "direction": "max",
+                            "is_enabled": false
+                        },
+                        "tablet": {
+                            "label": "Tablet Portrait",
+                            "value": 1024,
+                            "default_value": 1024,
+                            "direction": "max",
+                            "is_enabled": true
+                        },
+                        "tablet_extra": {
+                            "label": "Tablet Landscape",
+                            "value": 1200,
+                            "default_value": 1200,
+                            "direction": "max",
+                            "is_enabled": false
+                        },
+                        "laptop": {
+                            "label": "Laptop",
+                            "value": 1366,
+                            "default_value": 1366,
+                            "direction": "max",
+                            "is_enabled": false
+                        },
+                        "widescreen": {
+                            "label": "Widescreen",
+                            "value": 2400,
+                            "default_value": 2400,
+                            "direction": "min",
+                            "is_enabled": false
+                        }
+                    }
+                },
+                "version": "3.20.2",
+                "is_static": false,
+                "experimentalFeatures": {
+                    "e_optimized_assets_loading": true,
+                    "additional_custom_breakpoints": true,
+                    "container": true,
+                    "e_swiper_latest": true,
+                    "block_editor_assets_optimize": true,
+                    "ai-layout": true,
+                    "landing-pages": true,
+                    "e_image_loading_optimization": true
+                },
+                "urls": {
+                    "assets": "https:\/\/matchthemes.com\/demowp\/gleam\/wp-content\/plugins\/elementor\/assets\/"
+                },
+                "swiperClass": "swiper",
+                "settings": {
+                    "page": [],
+                    "editorPreferences": []
+                },
+                "kit": {
+                    "active_breakpoints": ["viewport_mobile", "viewport_tablet"],
+                    "global_image_lightbox": "yes",
+                    "lightbox_enable_counter": "yes",
+                    "lightbox_enable_fullscreen": "yes",
+                    "lightbox_enable_zoom": "yes",
+                    "lightbox_enable_share": "yes",
+                    "lightbox_title_src": "title",
+                    "lightbox_description_src": "description"
+                },
+                "post": {
+                    "id": 1125,
+                    "title": "Home%2011%20%E2%80%93%20Gleam",
+                    "excerpt": "",
+                    "featuredImage": "https:\/\/matchthemes.com\/demowp\/gleam\/wp-content\/uploads\/slider-1.jpg"
+                }
+            };
+            /*  */
+        </script>
+        <script data-optimized="1" src="https://matchthemes.com/demowp/gleam/wp-content/litespeed/js/84d90f813958f82f2a97baefe457680a.js?ver=7680a" id="elementor-frontend-js"></script>
